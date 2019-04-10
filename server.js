84a9ffcdee7923ca0ec39d371c750b4d9ca05752
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-04-10 18:08:20 +0200

    Add basic oauth flox

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const port = process.env.PORT || 8080;
 const baseUrl = 'https://sebprunier.mycozy.cloud'
 
 let token = null;
-
+let refreshToken = null;
+const redirectUri = 'http://127.0.0.1:8080/oauth/callback';
 
 function handleNoToken(req, res) {
-
+  const scope = 'io.cozy.apps:GET';
+  res.status(302).set('Location', `${baseUrl}/auth/login?scope=${scope}&client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`).send('');
 }
 
 app.use(bodyParser.json());
@@ -24,6 +26,22 @@ app.get('/', (req, res) => {
     fetch(`${baseUrl}/konnectors/`)
     res.type('html').send("<h1>Hello</h>")
   }
+});
+
+app.get('/oauth/token', (req, res) => {
+  const code = req.query.code;
+  fetch(`${baseUrl}/auth/authorize`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': '',
+      'Accept': 'application/json',
+    },
+    body: `code=${code}&grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=`
+  }).then(r => r.json()).then(resp => {
+    token = resp.access_token;
+    refreshToken= resp.refresh_token;
+    res.status(302).set('Location', `/`).send('');
+  });
 });
 
 app.use((err, req, res, next) => {

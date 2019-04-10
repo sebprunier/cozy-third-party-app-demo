501065380087889a8d523eb434fc71680ae83479
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-04-10 20:10:58 +0200

    Register client if needed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,56 +7,112 @@ const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const baseUrl = 'https://sebprunier.mycozy.cloud'
+const baseUrl = process.env.COZY_BASE_URL || 'https://sebprunier.mycozy.cloud';
 const redirectUri = 'http://127.0.0.1:8080/oauth/callback';
 const scope = 'io.cozy.triggers:GET io.cozy.jobs:GET';
 const state = '123456';
-const clientId = "747c208885084be978dcf3fdfb78be79";
-const clientSecret = "u7xfy7hXEGeVDruQx9Jak7PAozsWLNQQ";
 
-let token = null;
+let accessToken = null;
 let refreshToken = null;
+let clientConfig = null;
+
+function token() {
+  if (accessToken) {
+    const pl = JSON.parse(accessToken.split('.')[1]);
+    console.log('accessToken', pl);
+    return Promise.resolve(accessToken); // TODO: remove
+    if (pl.exp < Date.now()) {
+      return refreshAccessToken().then(() => accessToken);
+    } else {
+      return Promise.resolve(accessToken);
+    }
+  } else {
+    return Promise.resolve(null);
+  }
+}
 
 function handleNoToken(req, res) {
-  res.status(302).set('Location', `${baseUrl}/auth/authorize?state=${state}&scope=${scope}&client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&csrf_token=${state}`).send('');
+  res.status(302).set('Location', `${baseUrl}/auth/authorize?state=${state}&scope=${scope}&client_id=${clientConfig.clientId}&response_type=code&redirect_uri=${redirectUri}&csrf_token=${state}`).send('');
+}
+
+function registerOrLoadClient() {
+  try {
+    clientConfig = JSON.parse(fs.readFileSync('./client.json').toString('utf8'));
+    console.log('Loaded client config !');
+    return Promise.resolve(clientConfig);
+  } catch(e) {
+    console.log('Registering client ...');
+    return fetch(`${baseUrl}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "redirect_uris": ["http://127.0.0.1:8080/oauth/callback"],
+        "client_name": "cozy-third-party-app-demo",
+        "software_id": "github.com/sebprunier/cozy-third-party-app-demo"
+      })
+    }).then(r => r.json()).then(conf => {
+      clientConfig = conf;
+      fs.writeFileSync('./client.json', JSON.stringify(conf, null, 2));
+    });
+  }
+}
+
+function refreshAccessToken() {
+  return fetch(`${baseUrl}/auth/access_token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+    },
+    body: `state=${state}&refresh_token=${refreshToken}&grant_type=refresh_token&client_id=${clientConfig.clientId}&client_secret=${clientConfig.clientSecret}&redirect_uri=${redirectUri}&csrf_token=${state}`
+  }).then(r => r.json()).then(resp => {
+    console.log('refreshAccessToken', resp)
+    accessToken = resp.access_token;
+    refreshToken= resp.refresh_token;
+  });
 }
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  if (!token) {
-    handleNoToken(req, res);
-  } else {
-    fetch(`${baseUrl}/konnectors/`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer: ${token}`,
-        'Accept': 'application/vnd.api+json'
-      }
-    })
-    .then(r => r.json())
-    .then(json => {
-      console.log(json)
-      const html = JSON.stringify(json)
-      res.type('html').send(html)
-    })
-  }
+  return token().then(tok => {
+    if (!tok) {
+      handleNoToken(req, res);
+    } else {
+      fetch(`${baseUrl}/api/jobs/`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer: ${tok}`,
+          'Accept': 'application/vnd.api+json'
+        }
+      })
+      .then(r => r.json())
+      .then(json => {
+        console.log(json)
+        const html = JSON.stringify(json)
+        res.type('html').send(html)
+      })
+    }
+  });
 });
 
 app.get('/oauth/callback', (req, res) => {
   console.log(req.query)
   console.log(req.headers)
   const code = req.query.code;
-  fetch(`${baseUrl}/auth/authorize`, {
+  fetch(`${baseUrl}/auth/access_token`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Accept': 'application/json',
     },
-    body: `state=${state}&code=${code}&grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&csrf_token=${state}`
+    body: `state=${state}&code=${code}&grant_type=authorization_code&client_id=${clientConfig.clientId}&client_secret=${clientConfig.clientSecret}&redirect_uri=${redirectUri}`
   }).then(r => r.json()).then(resp => {
     console.log(resp)
-    token = resp.access_token;
+    accessToken = resp.access_token;
     refreshToken= resp.refresh_token;
     res.status(302).set('Location', `/`).send('');
   });
@@ -74,6 +131,8 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`cozy-demo listening on port ${port}!`);
+registerOrLoadClient().then(() => {
+  app.listen(port, () => {
+    console.log(`cozy-demo listening on port ${port}!`);
+  });
 });

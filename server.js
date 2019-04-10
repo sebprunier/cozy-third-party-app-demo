5e15857b5e448e1bab91aa6a9a1bfd48cefe6045
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-04-10 17:56:22 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.type('html').send("<h1>Hello</h>")
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err);
+    res.status(500).send({ error: err.message })
+  } else {
+    try {
+      next();
+    } catch(e) {
+      res.status(500).send({ error: e.message })
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`cozy-demo listening on port ${port}!`);
+});

befcfac8947c4a790fc614bd4d24d2ed3c2bef2d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-04-10 18:10:26 +0200

    Add basic oauth flox

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const baseUrl = 'https://sebprunier.mycozy.cloud'
+const redirectUri = 'http://127.0.0.1:8080/oauth/callback';
+const scope = 'io.cozy.apps:GET';
+const state = '123456';
 
 let token = null;
 let refreshToken = null;
-const redirectUri = 'http://127.0.0.1:8080/oauth/callback';
 
 function handleNoToken(req, res) {
-  const scope = 'io.cozy.apps:GET';
-  res.status(302).set('Location', `${baseUrl}/auth/login?scope=${scope}&client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`).send('');
+  res.status(302).set('Location', `${baseUrl}/auth/login?state=${state}&scope=${scope}&client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`).send('');
 }
 
 app.use(bodyParser.json());
@@ -33,10 +34,10 @@ app.get('/oauth/token', (req, res) => {
   fetch(`${baseUrl}/auth/authorize`, {
     method: 'POST',
     headers: {
-      'Content-Type': '',
+      'Content-Type': 'application/x-www-form-urlencoded',
       'Accept': 'application/json',
     },
-    body: `code=${code}&grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=`
+    body: `state=${state}&code=${code}&grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&csrf_token=123456`
   }).then(r => r.json()).then(resp => {
     token = resp.access_token;
     refreshToken= resp.refresh_token;

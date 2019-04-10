4cc74b3779e6e4b2b8da68c5e0d0cbdfe3b71a90
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-04-10 17:57:38 +0200

    Add handle no token

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 8080;
 
+let token = null;
+
+
+function handleNoToken(req, res) {
+
+}
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.type('html').send("<h1>Hello</h>")
+  if (!token) {
+    handleNoToken(req, res);
+  } else {
+    res.type('html').send("<h1>Hello</h>")
+  }
 });
 
 app.use((err, req, res, next) => {
